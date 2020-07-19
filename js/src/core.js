bf9b3680d3d3cd827a2b329da07f373c58dca78a
Srevin Saju <srevinsaju@sugarlabs.org>
2020-07-19 11:59:14 +0300

    Add emote Mapping dictionary from upstream

--- js/src/core.js
+++ js/src/core.js
@@ -1,6 +1,33 @@
 var globalVarSugaroid = "NULL";
 const BaseURL = "https://api-sugaroid.herokuapp.com";
 const BaseBackendURL = `${BaseURL}/chatbot`
+const emotionMapping = {
+    70: "sugaroid_genie",
+    0:  "sugaroid",
+    50: "sugaroid_cry",
+    11: "sugaroid_anger",
+    53: "sugaroid_non_expressive",
+    1:  "sugaroid_smile",
+    4:  "sugaroid_wink_left",
+    52: "sugaroid_non_expressive_left",
+    13: "sugaroid_extreme_anger",
+    5:  "sugaroid_cry",
+    15: "sugaroid_dead",
+    8:  "sugaroid_lol",
+    10: "sugaroid_depressed",
+    6:  "sugaroid_adorable",
+    14: "sugaroid_github",
+    12: "sugaroid_angel",
+    2:  "sugaroid_rich",
+    54: "sugaroid_seriously",
+    2:  "sugaroid_wink_right",
+    3:  "sugaroid_blush",
+    55: "sugaroid_depressed",
+    4:  "sugaroid_o",
+    51: "sugaroid_wink_left",
+    15: "sugaroid_dead",
+    57: "sugaroid_sleep"
+}
 
 
 function pingServer() {
